00ff4eb55593eba3b995c905086eb7a57eac8f1a
saul <saul@.hotmail.com>
2022-04-04 18:11:15 -0500

    variables

---
+++ 01-INTRODUCCION/js/01-variables.js
@@ -0,0 +1,18 @@
+var numero = 2;
+console.log('numero', numero);
+
+var palabra = 'palabra';
+console.log('palabra', palabra);
+
+var boleana = true;
+console.log('boleana', boleana);
+
+var colores = ['rojo','verde'];
+console.log('colores', colores);
+
+var persona = {
+    nombre:'saul',
+    apellido:'santamaria'
+}
+console.log('Personas ',persona);
+console.log('Personas Nombre ',persona.nombre);
