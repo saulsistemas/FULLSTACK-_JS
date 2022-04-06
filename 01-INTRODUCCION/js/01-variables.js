c4a6f0fc70cd3f881b330ff4a0746696d4daaebf
saul <saul@.hotmail.com>
2022-04-06 08:39:01 -0500

    variable DOM

--- 01-INTRODUCCION/js/01-variables.js
+++ 01-INTRODUCCION/js/01-variables.js
@@ -16,3 +16,17 @@ var persona = {
 }
 console.log('Personas ',persona);
 console.log('Personas Nombre ',persona.nombre);
+
+
+//VARIABLES DOM (MODELO DE OBJETOS DEL DOCUMENTO)
+
+var caja = document.querySelector('#caja');
+console.log("caja",caja);
+caja.style.width ="200px";
+caja.style.height ="200px";
+caja.style.background ="red";
+
+var cajas = document.querySelectorAll('.cajas');
+console.log("cajas",cajas);
+
+
