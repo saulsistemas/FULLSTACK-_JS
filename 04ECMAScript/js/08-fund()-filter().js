31e9795b2c3ed7f5d26b4b454c754fa47232b8a6
saul <saul@.hotmail.com>
2022-04-07 12:34:33 -0500

    funcion filter() y find()

---
+++ 04ECMAScript/js/08-fund()-filter().js
@@ -0,0 +1,23 @@
+//-----FUNCION FILTER
+//FILTRAR ARRAY U OBEJETO
+//SE NECESITA ENCERRAR EN VARIABLE PARA RETORNAR NUEVO ARRAY
+
+let vehiculos =[
+    {id:1,marca:"mazda",modelo:2018},
+    {id:2,marca:"toyota",modelo:2017},
+    {id:3,marca:"hyundai",modelo:2019},
+]
+let filtarVehiculos =vehiculos.filter(vehiculo=>{
+    return vehiculo.modelo>2017;
+});
+
+console.log(filtarVehiculos);
+
+//-----FUNCION FIND
+//RECORRE CADA ELEMENTO DEL ARRAY Y
+//DEVUELVE UN SOLO ELEMENTO
+
+let buscarVehiculo = vehiculos.find(vehiculo=>{
+    return vehiculo.modelo>2017;
+})
+console.log(buscarVehiculo);
