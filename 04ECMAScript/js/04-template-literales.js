5d0e99ef061c535290b0cb62aed64cb99c33eb26
saul <saul@.hotmail.com>
2022-04-07 10:56:39 -0500

    template literales

---
+++ 04ECMAScript/js/04-template-literales.js
@@ -0,0 +1,9 @@
+console.log("### TEMPLATE LITERALES ###");
+
+//CONCATENAR VARIABLES  DE FORMA DIFERENTE
+
+let estudiante = "saul";
+
+console.log("el estudiante se llama "+estudiante +" y juega futbol");
+
+console.log(`el estudiante se llama ${estudiante} y juega futbol`);
